058b7ecbf465b6e41790fb5826f9f94962b73c20
pigartpigaro <vichoirul@gmail.com>
2024-03-18 23:13:33 +0700

    c

--- sekwan/app/Http/Controllers/Transaksi/Transaksi_PerdinController.php
+++ sekwan/app/Http/Controllers/Transaksi/Transaksi_PerdinController.php
@@ -36,26 +36,36 @@ class Transaksi_PerdinController extends Controller
     //     ->paginate(request('per_page'));
     //     return new JsonResponse($perdin);
     // }
-    public function index(){
-        $perdin = Trans_rinci::latest('id')
-        ->with(['header'=>function($header){
-            $header->with(['kepmen50','provinsi','kota']);
-        },'uangharian'=>function($uh){
-            $uh->with(['tingkatan']);
-        },'penginapan'=>function($inap){
-            $inap->with(['provinsi','golongan']);
-        },'transportasi'=>function($trans){
-            $trans->with(['provinsi','kota']);
-        },'pesawat','taksi'=>function($taksi){
-            $taksi->with(['provinsi']);
-        },'dewan'])
+    public function index()
+    {
+
+        $perdin = Trans_Header::latest('id')
+        ->with(['kepmen50','provinsi','kota','rinci'=>function($rinci){
+            $rinci->with(['uangharian'=>function($uh){
+                $uh->with(['tingkatan']);
+            },'penginapan'=>function($inap){
+                $inap->with(['provinsi','golongan']);
+            },'transportasi'=>function($trans){
+                $trans->with(['provinsi','kota']);
+            },'pesawat','taksi'=>function($taksi){
+                $taksi->with(['provinsi']);
+            },'dewan']);
+        }])
         ->paginate(request('per_page'));
         return new JsonResponse($perdin);
     }
     public function storeheader(Request $request)
     {
         $post = new Trans_Header();
-        $post->fill($request->all());
+        $post->no_transaksi = self::buatnomor();
+        // $post->tanggal = self::buattanggal();
+        $post->tanggal = $request->date('Y/m/d');
+        $post->lamaperdin = $request->lamaperdin;
+        $post->judul = $request->judul;
+        $post->provinsi = $request->provinsi;
+        $post->kota = $request->kota;
+        $post->rekening50 = $request->rekening50;
+        // $post->uraian50 = $request->uraian50;
         $post->save();
 
         return response()->json(['message' => 'Berhasil di Simpan', 'data' => $post], 200);
@@ -63,10 +73,54 @@ class Transaksi_PerdinController extends Controller
 
     public function storerinci(Request $request)
     {
+
         $post = new Trans_rinci();
         $post->fill($request->all());
         $post->save();
 
         return response()->json(['message' => 'Berhasil di Simpan', 'data' => $post], 200);
+    }
+
+    public static function buatnomor(){
+        $huruf = ('SPM-PERDIN');
+        $no = ('4.02.0.00.0.00.01.0000');
+        date_default_timezone_set('Asia/Jakarta');
+        // $tgl = date('Y/m/d');
+        $thn = date('Y');
+        $rom = array('','I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII');
+        // $time = date('mis');
+        // $nomer=Transaksi::latest();
+        $cek = Trans_Header::count();
+        if ($cek == null){
+            $urut = "0001";
+            $sambung = $urut.'/'.strtoupper($no).'/'.strtoupper($huruf).'/'.$rom[date('n')].'/'.$thn;
+        }
+        else{
+            $ambil=Trans_Header::all()->last();
+            $urut = (int)substr($ambil->no_transaksi, 0, 4) + 1;
+            //cara menyambungkan antara tgl dn kata dihubungkan tnda .
+            // $urut = "000" . $urut;
+            if(strlen($urut) == 1){
+                $urut = "000" . $urut;
+            }
+            else if(strlen($urut) == 2){
+                $urut = "00" . $urut;
+            }
+            else if(strlen($urut) == 3){
+                $urut = "0" . $urut;
+            }
+            else {
+                $urut = (int)$urut;
+            }
+            $sambung = $urut.'/'.strtoupper($no).'/'.strtoupper($huruf).'/'.$rom[date('n')].'/'.$thn;
+        }
+
+        return $sambung;
+    }
+    public static function buattanggal(){
+        date_default_timezone_set('Asia/Jakarta');
+        $tanggal = date('Y-m-d');
+        return $tanggal;
+
     }
 }
