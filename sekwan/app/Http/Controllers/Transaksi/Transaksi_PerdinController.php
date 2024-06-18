1294ca33895a88b0032ca55332c80245fceab9c8
pigartpigaro <vichoirul@gmail.com>
2024-06-18 22:53:10 +0700

    ff

--- sekwan/app/Http/Controllers/Transaksi/Transaksi_PerdinController.php
+++ sekwan/app/Http/Controllers/Transaksi/Transaksi_PerdinController.php
@@ -79,6 +79,8 @@ class Transaksi_PerdinController extends Controller
             $post->judul = $request->judul;
             $post->provinsi = $request->id_propinsi;
             $post->kota = $request->id_kota;
+            $post->provinsi2 = $request->id_propinsi;
+            $post->kota2 = $request->id_kota;
             $post->komisi = $request->komisi;
             $post->rekening50 = '5.1.02.04.01.0001';
             $post->uraian50 = 'Belanja Perjalanan Dinas Biasa';
